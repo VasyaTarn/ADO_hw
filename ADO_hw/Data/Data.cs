53eea2220a0093c1238fdbbf4c06157cf9826749
VasyaTarn <vasatopktoneverittomuvlob@gmail.com>
2024-12-13 19:56:59 +0200

    Add project files.

---
+++ ADO_hw/Data/Data.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO_hw.Data
+{
+    internal class Data : DbContext
+    {
+        public DbSet<Entity.Department> Departments { get; set; }
+        public DbSet<Entity.Manager> Managers { get; set; }
+        public Data() : base()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=ado_db;Integrated Security=True"
+                );
+        }
+    }
+}
---
+++ ADO_hw/Data/Entity/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO_hw.Data.Entity
+{
+    internal class Department
+    {
+        public Guid Id { get; set; }
+        public String Name { get; set; } = null!;
+    }
+}
